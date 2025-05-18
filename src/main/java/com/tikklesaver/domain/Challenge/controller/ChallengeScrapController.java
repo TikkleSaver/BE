4992e5545768c30a26db49225915255940e7956a
heessunny <kimheesun107@gmail.com>
2025-05-18 16:44:53 +0900

    feat: 챌린지 스크랩 patch api 구현

---
+++ src/main/java/com/tikklesaver/domain/Challenge/controller/ChallengeScrapController.java
@@ -0,0 +1,28 @@
+package com.tikklesaver.domain.Challenge.controller;
+
+import com.tikklesaver.domain.Challenge.dto.challengeScrap.ChallengeScrapResponseDTO;
+import com.tikklesaver.domain.Challenge.entity.ChallengeScraped;
+import com.tikklesaver.domain.Challenge.service.ChallengeScrap.ChallengeScrapCommandService;
+import com.tikklesaver.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/challenges-scrap")
+public class ChallengeScrapController {
+
+    private final ChallengeScrapCommandService challengeScrapCommandService;
+
+    @PatchMapping("/{challengeId}/scrap")
+    @Operation(summary = "챌린지 스크랩 추가/삭제 API")
+    public ApiResponse<ChallengeScrapResponseDTO> challengeScrap(@PathVariable(name = "challengeId") Long challengeId){
+
+        //임시 memberId
+        Long memberId = 1L;
+        ChallengeScrapResponseDTO challengeScrap = challengeScrapCommandService.changeScrap(challengeId,memberId);
+        return ApiResponse.onSuccess(challengeScrap);
+    }
+
+}
--- src/main/java/com/tikklesaver/domain/Challenge/repository/ChallengeScrapRepository.java
+++ src/main/java/com/tikklesaver/domain/Challenge/repository/ChallengeScrapRepository.java
@@ -3,6 +3,9 @@ package com.tikklesaver.domain.Challenge.repository;
 import com.tikklesaver.domain.Challenge.entity.ChallengeScraped;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChallengeScrapRepository extends JpaRepository<ChallengeScraped, Long> {
     boolean existsByChallengeIdAndMemberId(Long challengeId, Long memberId);
+    Optional<ChallengeScraped> findByChallengeIdAndMemberId(Long challengeId, Long memberId);
 }
