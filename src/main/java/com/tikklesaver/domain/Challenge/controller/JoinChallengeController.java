a07ab9c88f59001c218e0c4684055fcd7a65cbcb
heessunny <kimheesun107@gmail.com>
2025-05-21 01:43:47 +0900

    refactor: JoinChallengeController 수정

--- src/main/java/com/tikklesaver/domain/Challenge/controller/JoinChallengeController.java
+++ src/main/java/com/tikklesaver/domain/Challenge/controller/JoinChallengeController.java
@@ -1,8 +1,10 @@
 package com.tikklesaver.domain.Challenge.controller;
 
 import com.tikklesaver.domain.Challenge.converter.ChallengeConverter;
+import com.tikklesaver.domain.Challenge.dto.challenge.ChallengeResponseDTO;
 import com.tikklesaver.domain.Challenge.dto.joinChallenge.JoinChallengeResponseDTO;
 import com.tikklesaver.domain.Challenge.entity.JoinChallenge;
+import com.tikklesaver.domain.Challenge.service.Challenge.ChallengeQueryService;
 import com.tikklesaver.domain.Challenge.service.JoinChallenge.JoinChallengeCommandService;
 import com.tikklesaver.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class JoinChallengeController {
 
     private final JoinChallengeCommandService joinChallengeCommandService;
+    private final ChallengeQueryService challengeQueryService;
 
     @PostMapping("/{challengeId}")
     @Operation(summary = "챌린지 참여 요청 API")
@@ -24,5 +27,17 @@ public class JoinChallengeController {
         Long memberId = 2L;
         JoinChallenge joinChallenge = joinChallengeCommandService.joinChallenge(memberId,challengeId);
         return ApiResponse.onSuccess(ChallengeConverter.toJoinChallengeResultDTO(joinChallenge));
+    }
+
+    @GetMapping("/{challengeId}")
+    @Operation(summary = "챌린지 신청 페이지 조회 API")
+    public ApiResponse<ChallengeResponseDTO.ChallengePreviewWithStatusResponseDTO> getChallengePreview(@PathVariable(name = "challengeId") Long challengeId){
+
+        Long memberId = 2L;
+
+        ChallengeResponseDTO.ChallengePreviewWithStatusResponseDTO challengePreview = challengeQueryService.getChallengePreview(memberId,challengeId);
+
+        return ApiResponse.onSuccess(challengePreview);
+
     }
 }
