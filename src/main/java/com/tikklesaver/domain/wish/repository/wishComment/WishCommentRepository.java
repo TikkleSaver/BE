567f07aa898f99613003764707de5f8c6c70be39
dokyung-kang <kangdk02@naver.com>
2025-05-26 11:11:53 +0900

    feat: 위시 댓글 조회 api 구현
    #82

--- src/main/java/com/tikklesaver/domain/wish/repository/wishComment/WishCommentRepository.java
+++ src/main/java/com/tikklesaver/domain/wish/repository/wishComment/WishCommentRepository.java
@@ -1,10 +1,13 @@
 package com.tikklesaver.domain.wish.repository.wishComment;
 
+import com.tikklesaver.domain.wish.entity.Wish;
 import com.tikklesaver.domain.wish.entity.WishComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface WishCommentRepository extends JpaRepository<WishComment, Long>, WishCommentRepositoryCustom {
-
+    
+    // 위시ID로 위시 댓글 찾기
+    List<WishComment> findAllByWish(Wish wish);
 }
---
+++ src/main/java/com/tikklesaver/domain/wish/service/wishComment/WishCommentQueryServiceImpl.java
@@ -0,0 +1,37 @@
+package com.tikklesaver.domain.wish.service.wishComment;
+
+import com.tikklesaver.domain.wish.converter.WishCommentConverter;
+import com.tikklesaver.domain.wish.dto.wishComment.WishCommentResponseDTO;
+import com.tikklesaver.domain.wish.entity.Wish;
+import com.tikklesaver.domain.wish.entity.WishComment;
+import com.tikklesaver.domain.wish.repository.wish.WishRepository;
+import com.tikklesaver.domain.wish.repository.wishComment.WishCommentRepository;
+import com.tikklesaver.global.apiPayload.code.status.ErrorStatus;
+import com.tikklesaver.global.apiPayload.exception.handler.WishCommentHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class WishCommentQueryServiceImpl implements WishCommentQueryService {
+
+    private final WishRepository wishRepository;
+    private final WishCommentRepository wishCommentRepository;
+
+    // 위시 댓글 목록 조회
+    @Override
+    public List<WishCommentResponseDTO.WishCommentPreviewDTO> getWishCommentList(Long memberId, Long wishId){
+
+        Wish wish = wishRepository.findById(wishId)
+                .orElseThrow(() -> new WishCommentHandler(ErrorStatus.WISH_NOT_FOUND));
+
+        List<WishComment> wishCommentList = wishCommentRepository.findAllByWish(wish);
+
+        return wishCommentList.stream()
+                .map(WishCommentConverter::toGetWishCommentResultDTO)
+                .toList();
+
+    }
+}
